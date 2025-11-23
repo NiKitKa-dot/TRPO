e2cd51cfaab2efe5d48fb4fbe94a8f88519ea56e
NiKitKa-dot <84975159+NiKitKa-dot@users.noreply.github.com>
2025-11-23 18:43:47 +0300

    Add files via upload

---
+++ Laba12/Class_Data.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DataTier
+{
+    public class Строение
+    {
+        public string Тип { get; set; }
+        public int Комнат { get; set; }
+        public double Метраж { get; set; }
+        public decimal Стоимость { get; set; }
+    }
+
+    public static class ВсеСтроения
+    {
+        public static List<Строение> ПолучитьВсеСтроения()
+        {
+            // Чтение данных из файла
+            var строкаи = File.ReadAllLines("buildings.txt");
+            var список = new List<Строение>();
+            foreach (var строка in строкаи)
+            {
+                var части = строка.Split('*');
+                список.Add(new Строение
+                {
+                    Тип = части[0],
+                    Комнат = int.Parse(части[1]),
+                    Метраж = double.Parse(части[2]),
+                    Стоимость = decimal.Parse(части[3])
+                });
+            }
+            return список;
+        }
+    }
+
+}
