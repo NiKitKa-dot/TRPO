48bc581e4f7bb6c53f2860cf1f3e4adb8d2cbb9c
NiKitKa-dot <84975159+NiKitKa-dot@users.noreply.github.com>
2025-11-23 18:41:29 +0300

    Add files via upload

---
+++ Laba12/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using LogicTier;
+
+namespace PresentationTier
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new АгентствоНедвижимости();
+        }
+
+        private void MainList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Можно добавить дополнительную логику при выборе элемента
+        }
+    }
+}
